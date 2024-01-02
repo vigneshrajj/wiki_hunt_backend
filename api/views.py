0661a70707f5b0af50c126b569c379ee56935221
Vignesh Raj <vigneshhrajj@gmail.com>
2024-01-02 13:00:10 +0530

    handle search from post request

--- api/views.py
+++ api/views.py
@@ -2,23 +2,28 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import requests
-from consumers import handle_search
+import json
 
-@api_view(['GET'])
-def search_article(request):
+def handle_search(query, limit, offset):
     try:
-        query = request.GET.get('query')
-        url = f"https://www.mediawiki.org/w/api.php?action=query&format=json&prop=info&iwurl=1&continue=&generator=search&formatversion=2&inprop=url&gsrsearch={query}&gsrlimit=10&gsroffset=0&gsrinfo=totalhits%7Csuggestion%7Crewrittenquery&gsrprop=snippet"
+        url = f"https://www.mediawiki.org/w/api.php?action=query&format=json&prop=info&iwurl=1&continue=&generator=search&formatversion=2&inprop=url&gsrsearch={query}&gsrlimit={limit}&gsroffset={offset}&gsrinfo=totalhits%7Csuggestion%7Crewrittenquery&gsrprop=snippet"
         res = requests.get(url)
         data = res.json()
+
         has_more = "continue" in data
         total_results = data["query"]["searchinfo"]["totalhits"]
-        articles = data["query"]["pages"]
-        return Response({ 'has_more': has_more, 'total_results': total_results, 'articles': articles })
+        articles = []
+
+        if "query" in data and "pages" in data["query"]:
+            articles = data["query"]["pages"]
+
+        return { 'has_more': has_more, 'total_results': total_results, 'articles': articles }
     except requests.exceptions.RequestException as e:
-        return e
+        return { 'error': e }
 
 @api_view(['POST'])
 def search(request):
-    print(request.body)
-    return Response({ 'message': 'works' })
+    body = json.loads(request.body.decode("utf-8"))
+    print(body)
+    response = handle_search(body['query'], body['limit'], body['offset'])
+    return Response(response)
