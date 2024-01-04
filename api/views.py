84e106dcb2f4c1504031c0b0609f0ef7e04283ef
Vignesh Raj <vigneshhrajj@gmail.com>
2024-01-04 13:48:27 +0530

    refactor: remove same function declared in multiple places

--- api/views.py
+++ api/views.py
@@ -1,25 +1,8 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-import requests
 import json
-
-def handle_search(query, limit, offset):
-    try:
-        url = f"https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info&iwurl=1&continue=&generator=search&formatversion=2&inprop=url&gsrsearch={query}&gsrlimit={limit}&gsroffset={offset}&gsrinfo=totalhits%7Csuggestion%7Crewrittenquery&gsrprop=snippet"
-        res = requests.get(url)
-        data = res.json()
-
-        has_more = "continue" in data
-        total_results = data["query"]["searchinfo"]["totalhits"]
-        articles = []
-
-        if "query" in data and "pages" in data["query"]:
-            articles = data["query"]["pages"]
-
-        return { 'has_more': has_more, 'total_results': total_results, 'articles': articles }
-    except requests.exceptions.RequestException as e:
-        return { 'error': e }
+from api.consumers import handle_search
 
 @api_view(['POST'])
 def search(request):
